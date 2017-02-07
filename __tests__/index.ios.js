b169d22c65130d6089b3fc67311a94e2f77265b6
Yao Bin Then <alvinthen890703@gmail.com>
2017-02-08 06:54:52 +0800

    Fix tests

--- __tests__/index.ios.js
+++ __tests__/index.ios.js
@@ -4,7 +4,7 @@ import React from 'react';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-import Index from '../index.ios';
+import Index from '..';
 
 it('renders correctly', () => {
   const tree = renderer.create(
--- index.android.js
+++ index.android.js
@@ -2,4 +2,6 @@ import { AppRegistry } from 'react-native';
 
 import App from './src/App';
 
+export default App;
+
 AppRegistry.registerComponent('ReactNativeStarterPack', () => App);
